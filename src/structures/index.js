bc3521cd8721bfebb516a7e76ced73ff8f949532
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-10-29 21:08:28 -0400

    added an index to the structures folder

---
+++ src/structures/index.js
@@ -0,0 +1,13 @@
+import Account from './Account';
+import Coin from './Coin';
+import CoinInstance from './CoinInstance';
+import Series from './Series';
+import Setting from './Setting';
+
+export {
+  Account,
+  Coin,
+  CoinInstance,
+  Series,
+  Setting,
+};
