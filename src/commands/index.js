17d4dd178eaba3e16329e3c79d0e098fa94e9867
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-10-30 15:31:13 -0400

    added the individual exports to index exports

---
+++ src/commands/index.js
@@ -0,0 +1,19 @@
+import Command from './Command';
+import { process } from './process';
+import {
+  findCommand,
+  commandsAll,
+  commandsAdmin,
+  commandsRegistered,
+  commandsUnregistered,
+} from './organiser';
+
+export {
+  Command,
+  process,
+  findCommand,
+  commandsAll,
+  commandsAdmin,
+  commandsRegistered,
+  commandsUnregistered,
+};
