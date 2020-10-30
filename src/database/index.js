f8d76ebc838d61929e39dd286c5f9070fc05fbbf
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-10-30 15:35:15 -0400

    moved database client to seperate file

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,6 @@
-import { MongoClient } from 'mongodb';
+import { client, database } from './client';
 
-const uri = 'mongodb+srv://Soggy:ripazTXlhg7fhm1o@clusterdev.73yxe.mongodb.net/collector?retryWrites=true&w=majority';
-
-export const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-export const database = async function mongoDatabase() {
-  await client.connect();
-  return client.db('collector');
+export {
+  client,
+  database,
 };
