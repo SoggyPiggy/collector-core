dc4063b0756cd8462995ddec0939cfc5ad34042c
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-10-30 14:47:15 -0400

    will probably move to using  arguments instead

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -11,11 +11,36 @@
  * @property {boolean} [registeredAccess] Whether the command can be accessed by registered users
  * @property {boolean} [unregisteredAccess] Whether the command can be accessed by non-registered
  * users
+ * @property {CommandArgumentOptions[]} [arguments] The options that will be used to create the
+ * parser
  * @property {string[]} [argsStrict] The arg's main name
  * @property {string[]} [argsAliases] The arg's aliases
  * @property {string[]} [argsDescriptions] The arg's description
  * @property {function} [onExecute] The function that will be called when a command's execute
  * function is called
+ */
+
+/**
+ * Options for a Command Argument.
+ * @typedef {Object} CommandArgumentOptions
+ * @property {stritng} [name] Give the option name. The name is the key for the parsed opts object
+ * @property {stritng[]} [names] These give the option name and aliases. The first name is the key
+ * for the parsed opts object
+ * @property {string|'bool'|'string'|'number'|'integer'|'positiveInteger'|'date'|'arrayOfBool'|
+ * 'arrayOfString'|'arrayOfNumber'|'arrayOfInteger'|'arrayOfPositiveInteger'|'arrayOfDate'} [type]
+ * The type the option should be
+ * @property {*} [default] A default value used for this option, if the option isn't specified in
+ * argv.
+ * @property {string} [completionType] This is used for Bash completion for an option argument.
+ * If not specified, then the value of type is used.
+ * @property {string|string[]} [env] An environment variable name (or names) that can be used as a
+ * fallback for this option.
+ * @property {string} [help] Used for parser.help() output.
+ * @property {string} [helpArg] Used in help output as the placeholder for the option argument.
+ * @property {boolean} [helpWrap] Default true. Set this to false to have that option's help not be
+ * text wrapped in <parser>.help() output.
+ * @property {boolean} [hidden] Default false. If true, help output will not include this option.
+ * See also the includeHidden option to bashCompletionFromOptions() for Bash completion.
  */
 
 export default class Command {
@@ -32,6 +57,7 @@ export default class Command {
     this.isPublic = true;
     this.registeredAccess = true;
     this.unregisteredAccess = false;
+    this.arguments = [];
     this.argsStrict = [];
     this.argsAliases = [];
     this.argsDescriptions = [];
