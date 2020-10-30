6819772002c78d48e0c6b44172bc0ac1a7f1dba9
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-10-30 01:36:54 -0400

    The consumer module for discord

---
+++ src/discord/consumer.js
@@ -0,0 +1,24 @@
+import { DMChannel } from 'discord.js';
+import { process } from '../commands';
+import client from './client';
+import { send } from './speaker';
+
+/**
+ * @typedef {import('discord.js').Message} Message
+ */
+
+/**
+ * Checks if message is a valid command and sends it to the commands module if it is.
+ * @param {Message} message
+ */
+const processMessage = async function processMessageFromClientEvent(message) {
+  const { author, channel, content } = message;
+  if (author.bot) return;
+  if (!(channel instanceof DMChannel || content.startsWith('>'))) return;
+  const input = content.toLowerCase().replace(/^>/g, '');
+  process(input, message)
+    .then((...replyData) => send(...replyData))
+    .catch(() => {});
+};
+
+client.on('message', processMessage);
