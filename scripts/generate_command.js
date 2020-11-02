1350d9c5b0507ae01052787514b7ab883d1e738d
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-01 22:39:44 -0500

    moved to a separate callable module

--- scripts/generate_command.js
+++ scripts/generate_command.js
@@ -1,24 +1,6 @@
 /* eslint-disable no-console */
-import { join } from 'path';
-import fs from 'fs';
+import generator from '../src/commands/generator';
 
-const parts = process.argv.slice(2, Infinity);
-const title = parts.map((part) => part[0].toUpperCase() + part.slice(1, Infinity).toLowerCase());
-const name = parts.join('_').toLowerCase();
-const file = join(process.cwd(), './src/commands/commands/', `${name}.js`);
-fs.writeFile(file,
-  `import Command from '../Command';
-
-const execute = async function executeCommand({ message, account }) {
-};
-
-const command = new Command({
-  id: '${name}',
-  title: '${title}',
-  description: '',
-  aliases: ['${name}'],
-  onExecute: execute,
-});
-
-export default command;
-`, { flag: 'wx' }, console.warn);
+generator(process.argv.slice(2, Infinity).join(' '))
+  .catch(console.warn)
+  .then(console.log);
---
+++ src/commands/generator.js
@@ -0,0 +1,33 @@
+import { join } from 'path';
+import fs from 'fs';
+
+const generateCommand = async function generateCommandFile(data) {
+  const parts = data.split(' ');
+  const title = parts.map((part) => part[0].toUpperCase() + part.slice(1, Infinity).toLowerCase());
+  const name = parts.join('_').toLowerCase();
+  const file = join(process.cwd(), './src/commands/commands/', `${name}.js`);
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file,
+      `import Command from '../Command';
+        
+        const execute = async function executeCommand({ message, account }) {
+        };
+        
+        const command = new Command({
+          id: '${name}',
+          title: '${title}',
+          description: '',
+          aliases: ['${name}'],
+          examples: ['${name}'],
+          onExecute: execute,
+        });
+        
+        export default command;
+        `.replace(/^ {8}/gm, ''), { flag: 'wx' }, (error) => {
+        if (error) reject(error);
+        else resolve(`Command file generated: ${file}`);
+      });
+  });
+};
+
+export default generateCommand;
