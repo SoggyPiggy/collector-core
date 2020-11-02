8e29f6a2ee2f2dcd28f936af40e43c8a02575927
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-02 00:50:26 -0500

    Made some helper functions for the database

---
+++ src/database/helpers.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-param-reassign */
+export const insertOne = async function dbHelperInsertOne(collection, obj) {
+  collection = await collection;
+  return new Promise((resolve, reject) => {
+    collection.insertOne(obj)
+      .catch(reject)
+      .then(({ insertedId }) => resolve({ ...obj, _id: insertedId }));
+  });
+};
+
+export const updateOne = async function dbHelperUpdateOne(collection, obj, options) {
+  collection = await collection;
+  return new Promise((resolve, reject) => {
+    collection.updateOne({ _id: obj._id }, { $set: obj }, options)
+      .catch(reject)
+      .then(() => resolve(obj));
+  });
+};
