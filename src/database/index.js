2b3e9b89388ef0be76fe9b4703017e58b95f7383
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-02 10:08:36 -0500

    added helpers to the database/index

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,14 @@
 import { client, database } from './client';
+import {
+  insertOne,
+  updateOne,
+  fineOne,
+} from './helpers';
 
 export {
   client,
   database,
+  insertOne,
+  updateOne,
+  fineOne,
 };
