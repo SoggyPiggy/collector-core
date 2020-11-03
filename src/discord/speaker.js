ca7bdf2afa4799326c6ceb88ae3e8aabd6917e34
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-03 15:33:05 -0500

    speaker now handles some instances of returns

--- src/discord/speaker.js
+++ src/discord/speaker.js
@@ -1,36 +1,65 @@
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../commands';
 import {
-  Account,
-  Coin,
-  CoinInstance,
-  Series,
 } from '../structures';
 
-export const resolveMessage = function resolveMessageFromContentPassed(content) {
+/**
+ * @param {Command} command
+ */
+const formatListCommand = function formatListCommand(command) {
+  return `**${command.title}**: \`${command.aliases[0]}\` ${command.description}`;
+};
+
+const resolveContentArrayItem = function resolveContentArrayItem(item) {
   switch (true) {
-    case content instanceof Error:
-      return content.message;
-    case content instanceof Account:
-    case content instanceof Coin:
-    case content instanceof CoinInstance:
-    case content instanceof Series:
+    case (typeof item === 'string'):
+      return item;
+    case (item instanceof Command):
+      return formatListCommand(item);
     default:
-      return ':)';
+      return 'Could not resolve list item';
   }
 };
 
-export const resolveDestination = function resolveDestinationFromDataPassed(target, { message }) {
-  switch (target) {
-    case 'reply':
-      return message.channel;
-    case 'notify':
-    case 'direct':
+const resolveContentArray = function resolveContentArray(content) {
+  const [options, ...items] = content;
+  const embed = new MessageEmbed({ ...options });
+  embed.setDescription(items.map(resolveContentArrayItem));
+  return embed;
+};
+
+/**
+ * @param {Command} command
+ */
+const formatCommand = function formatCommand(command) {
+  const embed = new MessageEmbed();
+  embed.setTitle(`Help Menu: ${command.title}`);
+  embed.setDescription([
+    command.description,
+    `\n**Aliases**\n\`${command.aliases.join('`, `')}\``,
+    // TODO: Add command.arguments here
+    `\n**Examples**\n${command.examples.join('\n')}`,
+  ].filter((section) => section !== '').join('\n'));
+  return embed;
+};
+
+const resolveContent = function resolveContent(content) {
+  switch (true) {
+    case (typeof content === 'string'):
+    case (content instanceof MessageEmbed):
+      return content;
+    case (Array.isArray(content)):
+      return resolveContentArray(content);
+    case (content instanceof Command):
+      return formatCommand(content);
     default:
-      return message.author;
+      return 'Could not resolve item';
   }
 };
 
-export const send = function sendMessage(content, target, data) {
-  const message = typeof content === 'string' ? content : resolveMessage(content, data);
-  const destination = resolveDestination(target, data);
+export const send = function sendMessage(content, destination) {
+  const message = resolveContent(content);
   destination.send(message);
 };
+
+export const notify = function sendNotification() {};
