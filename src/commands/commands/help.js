0879f05e49522bc365bdd76a730034415085cc2f
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-03 15:30:30 -0500

    help now supports command arguments

--- src/commands/commands/help.js
+++ src/commands/commands/help.js
@@ -1,13 +1,19 @@
 import Command from '../Command';
 import { commandsAdmin, commandsRegistered, commandsUnregistered } from '..';
+import { findCommand } from '../organiser';
 
 /**
  * @param {import('../').CommandExecuteArgs} commandExecuteArgs
  */
-const execute = async function executeCommand({ account }) {
-  if (typeof account === 'undefined') return commandsUnregistered;
-  if (account.isAdmin) return commandsAdmin;
-  return commandsRegistered;
+const execute = async function executeCommand({ account, inputArguments }) {
+  const [argCommand] = inputArguments;
+  let commands;
+  if (typeof account === 'undefined') commands = commandsUnregistered;
+  else if (account.isAdmin) commands = commandsAdmin;
+  else commands = commandsRegistered;
+  const command = findCommand(argCommand, commands);
+  if (command instanceof Command) return command;
+  return [{ title: 'Help Menu' }, ...commands];
 };
 
 const command = new Command({
