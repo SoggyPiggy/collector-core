0f249b49bbd091b543a0a4e337a1db8cf6a190a7
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-08 20:46:09 -0500

    register up to date

--- src/commands/commands/register.js
+++ src/commands/commands/register.js
@@ -1,10 +1,18 @@
+import { Account } from '../../structures';
 import Command from '../Command';
 
 /**
  * @param {import('../').CommandExecuteArgs} commandExecuteArgs
  */
-const execute = async function executeCommand() {
-  throw new Error('Command execute function not defined');
+const execute = async function executeCommand({ account, discordUser }) {
+  if (typeof account !== 'undefined') throw new Error('Account has already been registered');
+  if (typeof discordUser !== 'undefined') {
+    return Account.new({
+      discordID: discordUser.id,
+      discordUsername: discordUser.username,
+    });
+  }
+  throw new Error('Register failed to create account');
 };
 
 const command = new Command({
@@ -13,6 +21,7 @@ const command = new Command({
   description: 'Registers yourself to the bot',
   aliases: ['register'],
   unregisteredAccess: true,
+  registeredAccess: false,
   onExecute: execute,
 });
 
