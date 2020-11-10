806a4eb05b4de9f48942b8b98ac0385dcf8911b2
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-10 15:56:32 -0500

    uses new changelog imports, has full functionality

--- src/commands/commands/patches.js
+++ src/commands/commands/patches.js
@@ -1,19 +1,36 @@
 import Command from '../Command';
-import { getLatest } from '../../changelog';
+import { versions } from '../../changelog';
 
 /**
  * @param {import('../').CommandExecuteArgs} commandExecuteArgs
  */
-const execute = async function executeCommand() {
-  return getLatest();
+const execute = async function executeCommand({ inputArguments, command }) {
+  const argv = command.parseArgs(inputArguments);
+  const { previous, list } = argv;
+  if (list) return [{ argv, embed: { title: 'Change Logs' } }, ...versions];
+  return versions[previous];
 };
 
 const command = new Command({
   id: 'patches',
-  title: 'Patches',
+  title: 'Patch Notes',
   category: 'Utility',
   description: 'Displays the latest patch notes and changelog',
   aliases: ['patch', 'patchnotes'],
+  arguments: [{
+    names: ['previous', 'prev'],
+    type: 'positiveInteger',
+    default: 0,
+    help: 'How many changelogs back ago',
+  }, {
+    names: ['list', 'l'],
+    type: 'bool',
+    default: false,
+    help: 'Should display a list of patchnotes',
+  }, {
+    ...Command.pageArg,
+    help: `If list is used, ${Command.pageArg.help.toLowerCase()}`,
+  }],
   onExecute: execute,
 });
 
