8011d5380cab19994d90170aad8a6decae972f8c
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-15 19:15:29 -0500

    now functional

--- src/commands/commands/suggest.js
+++ src/commands/commands/suggest.js
@@ -1,10 +1,14 @@
 import Command from '../Command';
+import { Suggestion } from '../../structures';
 
 /**
  * @param {import('../').CommandExecuteArgs} commandExecuteArgs
  */
-const execute = async function executeCommand() {
-  throw new Error('Command execute function not defined');
+const execute = async function executeCommand({ account, inputArguments }) {
+  const content = inputArguments.join(' ');
+  if (content.length <= 0) throw new Error('No suggestion was given');
+  const suggestion = await Suggestion.new(account, content);
+  return suggestion;
 };
 
 const command = new Command({
