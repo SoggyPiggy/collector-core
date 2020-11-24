da50c0af5e45d5fd7f965cc27a65baea1d833b40
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-11-24 02:04:20 -0500

    removed old structure

--- src/renderer/utils.js
+++ src/renderer/utils.js
@@ -1,23 +1,4 @@
 import { join } from 'path';
-import { CoinInstance } from '../structures';
 
 export const artPath = join(process.cwd(), '/assets/coins/art');
 export const mapPath = join(artPath, '../maps');
-
-/* eslint-disable no-await-in-loop */
-/**
- * @param {import('../structures').Coin|import('../structures').Series} item
- * @param {String} scope
- * @returns {Array}
- */
-export const getStructure = async function getDirectoryStructureOfCoinOrSeries(item, scope) {
-  let structure = [];
-  let step = item instanceof CoinInstance ? (await item.coin) : item;
-  while (step !== undefined) {
-    structure.unshift(step);
-    step = await step.series;
-  }
-  if (scope !== undefined) structure = structure.map((value) => value[scope]);
-  return structure;
-};
-/* eslint-enable no-await-in-loop */
