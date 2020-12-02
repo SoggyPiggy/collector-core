66717530b2ed8aab3b499517a43a73a6208194ad
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-12-02 02:28:07 -0500

    add discord/reactive The first steps of making messages reactive

---
+++ src/discord/reactive.js
@@ -0,0 +1,63 @@
+const reactiveCache = new Map();
+const timeoutCache = new Map();
+
+const resetRemoval = function setOrResetRemovalOfReactives(id) {
+  const old = timeoutCache.get(id);
+  if (old) {
+    clearTimeout(old);
+    timeoutCache.delete(id);
+  }
+  const timeout = setTimeout(() => {
+    reactiveCache.delete(id);
+  }, 30000);
+  timeoutCache.set(id, timeout);
+};
+
+/**
+ * @param {function} resolver
+ * @param {Promise<import('discord.js').Message>} message
+ * @param {*} content
+ */
+export const makeReactive = async function makeMessageReactive(resolver, message, content, user) {
+  if (!Array.isArray(content)) return;
+  resetRemoval((await message).id);
+  reactiveCache.set((await message).id, {
+    resolver,
+    content,
+    user,
+  });
+  (await message).react('⬅️');
+  (await message).react('➡️');
+};
+
+/**
+ * @param {import('discord.js').MessageReaction} reactionMessage
+ * @param {import('discord.js').User} reactionUser
+ */
+export const processReaction = async function processReactionForReactives(
+  reactionMessage,
+  reactionUser,
+) {
+  const { message, emoji } = reactionMessage;
+  if (!reactiveCache.has(message.id)) return;
+  const { resolver, content, user } = reactiveCache.get(message.id);
+  if (reactionUser.id !== user.id) return;
+  resetRemoval(message.id);
+  const [options, ...items] = content;
+  const { argv = {} } = options;
+  let { page = 1 } = argv;
+  if (emoji.name === '⬅️') page -= 1;
+  if (emoji.name === '➡️') page += 1;
+  page = Math.max(1, Math.min(page, Math.ceil(items.length / 20)));
+  const newContent = [{ ...options, argv: { ...argv, page } }, ...items];
+  const newResolvedContent = await resolver(newContent);
+  reactiveCache.set(message.id, {
+    resolver,
+    content: newContent,
+    user,
+  });
+  if (page !== argv.page) message.edit(newResolvedContent);
+};
+
+export const removeAll = function removeAllReactivesAndReactions() {
+};
