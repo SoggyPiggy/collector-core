d76d7e753b0288f5761b63654e28ae93f5cdb678
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-12-02 00:33:30 -0500

    fix execute not checking dates

--- src/claims/claims/doomsday.js
+++ src/claims/claims/doomsday.js
@@ -1,13 +1,16 @@
-import { Coin, CoinInstance } from '../../structures';
+import { Coin, CoinInstance, Setting } from '../../structures';
 import { ClaimLogger } from '../../loggers';
 
 export const hash = '828a871294bf08124b4abbb646cd04f10d65a1bfd7621c16a54b62365d7b75d0';
+const startDate = new Date(2020, 10, 1);
+const endDate = Setting.get('claim-doomsday-endDate');
 
 /**
  * @param {import('../../commands').CommandExecuteArgs} commandExecuteArgs
  */
 const execute = async function executeClaimFunction({ account }) {
-  const coins = await Promise.all(Array(2).fill(null).map(() => Coin.randomCollectable()));
+  const quantity = Math.floor(((await endDate) - startDate) / 86400000);
+  const coins = await Promise.all(Array(quantity).fill(null).map(() => Coin.randomCollectable()));
   const coinInstances = await CoinInstance.insertBulk(
     coins.map((coin) => CoinInstance.generate({}, account, coin)),
   );
