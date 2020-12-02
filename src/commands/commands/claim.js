a5abc39abf75da32dcf0ed5c29166d9ea64f32eb
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-12-01 23:54:48 -0500

    uses claims/ now

--- src/commands/commands/claim.js
+++ src/commands/commands/claim.js
@@ -1,26 +1,13 @@
-import crypto from 'crypto';
-import CollectorError from '../../error/CollectorError';
 import Command from '../Command';
-
-export const generateHash = function generateSecureHashForCodes(code) {
-  return crypto.createHmac('sha256', process.env.HASH_KEY).update(code).digest('hex');
-};
+import { processClaim } from '../../claims';
 
 /**
  * @param {import('../').CommandExecuteArgs} commandExecuteArgs
  */
-const execute = async function executeCommand({ inputArguments }) {
+const execute = async function executeCommand(commandExecuteArgs) {
+  const { inputArguments } = commandExecuteArgs;
   const [code] = inputArguments;
-  const hash = generateHash(code);
-  switch (hash) {
-    // Doomsday code
-    case '828a871294bf08124b4abbb646cd04f10d65a1bfd7621c16a54b62365d7b75d0':
-      throw new CollectorError('No doomsday code support yet');
-    // Leaderboard battle code
-    case '0d25f181ae2dfc226eaa0f83d9c4a967bd3993be4cfe54a0fda7f62b53467810':
-      throw new CollectorError('No code support yet');
-    default: throw new CollectorError(`Unknown claimable code: ${code}`);
-  }
+  return processClaim(code, commandExecuteArgs);
 };
 
 const command = new Command({
