db900d755ceaee00064f3eab6d504715ddecbbde
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-12-01 23:27:37 -0500

    Add missing jsdoc

--- src/commands/index.js
+++ src/commands/index.js
@@ -21,7 +21,10 @@ export {
 /**
  * @typedef {Object} CommandExecuteArgs
  * @property {import('../structures').Account | undefined} [account]
+ * @property {Command} [command]
  * @property {string} [input]
  * @property {string} [inputCommand]
  * @property {string[]} [inputArguments]
+ * @property {import('discord.js').Message | undefined} [discordMessage]
+ * @property {import('discord.js').User | undefined} [discordUser]
  */
