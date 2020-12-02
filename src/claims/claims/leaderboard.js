c507e703656b9046662c0fce66dc4fe8da26af17
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-12-01 23:36:07 -0500

    add claims/leaderboard

---
+++ src/claims/claims/leaderboard.js
@@ -0,0 +1,8 @@
+import CollectorError from '../../error';
+
+export const hash = '0d25f181ae2dfc226eaa0f83d9c4a967bd3993be4cfe54a0fda7f62b53467810';
+
+const execute = function executeClaimFunction() {
+  throw new CollectorError('Code support not implemented');
+};
+export default execute;
