107a03ba4f0f5b6ff8b55ef8bd61b72a045e8aca
SoggyPiggy <soggypiggy@gmail.com>
2020-12-20 14:35:32 -0500

    add eval...Coin eval...CoinInstance to utils/index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,13 @@
 import random from './random';
 import userResolver from './userResolver';
 import generateHash from './generateHash';
+import evaluateCoin from './evaluateCoin';
+import evaluateCoinInstance from './evaluateCoinInstance';
 
 export {
   generateHash,
   userResolver,
+  evaluateCoin,
+  evaluateCoinInstance,
   random,
 };
