b7354dc17eee4894e4b2970f777cb1710742ac0e
SoggyPiggy <soggypiggy@gmail.com>
2020-12-20 14:37:30 -0500

    tweak uses utils now

--- src/scheduler/tasks/evaluate_coins.js
+++ src/scheduler/tasks/evaluate_coins.js
@@ -1,39 +1,12 @@
 import scheduler from 'node-schedule';
 import { Account, Coin, CoinInstance } from '../../structures';
+import { evaluateCoin, evaluateCoinInstance } from '../../utils';
 
 const getAll = async function getAllOfSomething(Class) {
   return [...(await (await Class.collection)
     .find({})
     .map((document) => new Class(document))
     .toArray())];
-};
-
-const evaluateInstance = function evaluteCoinInstanceValue(options = {}) {
-  const { coin, instances } = options;
-  return {
-    ...options,
-    instances: instances.map((instance) => {
-      let { value } = coin;
-      if (instance.condition === 0) value *= 0.5;
-      else value *= (0.9 * instance.condition) + 0.1;
-      if (instance.isAltered) value *= 0.8;
-      return new CoinInstance({ ...instance, value });
-    }),
-  };
-};
-
-const evaluateCoin = function evaluteCoinValue(options = {}) {
-  const { coin, accounts, instances } = options;
-  const owners = [...new Set(instances.map(({ _accountID }) => _accountID)).values()].length;
-  let value = ((1000 / coin.weight) ** 3) * 5;
-  if (accounts.length !== owners) value *= (0.5 * ((owners - 1) / accounts.length)) + 0.5;
-  if (!coin.inCirculation) value *= 1.2;
-  if (coin.isRetired) value *= 1.2;
-  if (coin.isError) value *= 1.8;
-  return {
-    ...options,
-    coin: new Coin({ ...coin, value }),
-  };
 };
 
 const evaluateCoins = async function evaluteAllCoinValues() {
@@ -46,8 +19,21 @@ const evaluateCoins = async function evaluteAllCoinValues() {
       accounts,
       instances: coinInstances.filter((instance) => coin._id.equals(instance._coinID)),
     }))
-    .map(evaluateCoin)
-    .map(evaluateInstance)
+    .map((options) => ({
+      ...options,
+      coin: new Coin({
+        ...options.coin,
+        value: evaluateCoin(options.coin, accounts, options.instances),
+      }),
+    }))
+    .map((options) => ({
+      ...options,
+      instances: options.instances
+        .map((instance) => new CoinInstance({
+          ...instance,
+          value: evaluateCoinInstance(options.coin, instance),
+        })),
+    }))
     .filter(({ coin }, index) => coin.value !== coins[index].value);
   Coin.updateBulk(coinsModified.map(({ coin }) => coin));
   CoinInstance.updateBulk(coinsModified.map(({ instances }) => instances).flat());
